254102adc430916905276809a92bcebcb294d390
Serasb <sera.baygan@sabanciuniv.edu>
2025-11-24 12:58:14 +0300

    Update add_habit_screen.dart

--- lib/screens/add_habit_screen.dart
+++ lib/screens/add_habit_screen.dart
@@ -1,15 +1,1 @@
-import 'package:flutter/material.dart';
 
-class AddHabitScreen extends StatelessWidget {
-  const AddHabitScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Add Habit')),
-      body: const Center(
-        child: Text('Add Habit Form goes here'),
-      ),
-    );
-  }
-}
